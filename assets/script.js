251d7520c65beb2085b24ea17b61249b28f4490e
Amy <amyemoretti@gmail.com>
2019-01-24 10:14:02 -0600

    Gifs, HTML finished, JS begun

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const catArray = [
+    "1.gif", 
+    "2.gif", 
+    "3.gif", 
+    "4.gif", 
+    "5.gif", 
+    "6.gif", 
+    "7.gif", 
+    "8.gif", 
+    "9.gif", 
+    "10.gif", 
+    "11.gif", 
+    "12.gif", 
+    "13.gif", 
+    "14.gif"
+];
+
+const path = "/assets/gifs/";
+const catDiv = document.querySelector(".cat");
+
